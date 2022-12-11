33521fe1d12f3c95b0a2527326e85f7e74ae931c
Daniel Tlach <daniel@tlach.cz>
2022-12-11 22:42:53 +0100

    Fixes two issues reported by Sonar

--- tests/SmsMessageTest.php
+++ tests/SmsMessageTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace SimPod\SmsManager\Tests;
 
 use PHPUnit\Framework\TestCase;
-use SimPod\SmsManager\Exception\NoRecipientsProvided;
 use SimPod\SmsManager\RequestType;
 use SimPod\SmsManager\SmsMessage;
 
@@ -25,13 +24,6 @@ final class SmsMessageTest extends TestCase
         self::assertSame($expectedRequestType->getValue(), $smsMessage->getRequestType()->getValue());
         self::assertSame($sender, $smsMessage->getSender());
         self::assertSame($customId, $smsMessage->getCustomId());
-    }
-
-    public function testNoRecipientsThrowException(): void
-    {
-        $this->expectException(NoRecipientsProvided::class);
-
-        new SmsMessage('', []);
     }
 
     /** @return mixed[][] */
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ use function error_reporting;
 
 use const E_ALL;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
